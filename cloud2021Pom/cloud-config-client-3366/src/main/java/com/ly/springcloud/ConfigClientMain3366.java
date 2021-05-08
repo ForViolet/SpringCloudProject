edd44e2e6d0e431c2e2c48f7ccf85179882ca3eb
Blanc <yijianhuawu@163.com>
2021-05-08 11:20:35 +0800

    SpringCloud Bus消息总线案例完成

---
+++ cloud2021Pom/cloud-config-client-3366/src/main/java/com/ly/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.ly.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: Echo
+ * @Date: 2021/5/7-18:21
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
